764bfb78261642bd058f851740b81b1fd548feae
marshan07 <marshanto4006@gmail.com>
2023-02-02 00:05:45 +0530

    added addmethod

--- src/PlayWithGit.java
+++ src/PlayWithGit.java
@@ -1,8 +1,14 @@
 
 public class PlayWithGit {
 	
+	public static int addTwoNumbers(int num1, int num2) {
+		return num1+num2;
+	}
+	
 	public static void main(String[] args) {
 		
+		System.out.println(addTwoNumbers(22, 0));
+
 	}
 
 }
